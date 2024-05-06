4e4840f7e179a708bef6513b0e69fe9ba5b20391
Bhaskar Mishra <bmishra23012002@gmail.com>
2024-05-06 16:17:16 +0530

    added logs

--- server/src/utils/verifyUser.js
+++ server/src/utils/verifyUser.js
@@ -3,11 +3,11 @@ import { errorHandler } from './error.js';
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) {
-    return next(errorHandler(401, 'Unauthorized'));
+    return next(errorHandler(401, 'Unauthorized token not found in cookie'));
   }
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
-      return next(errorHandler(401, 'Unauthorized'));
+      return next(errorHandler(401, 'Unauthorized , error verifying user jwt'));
     }
     req.user = user;
     next();
